0bf9c1083e2bdb0d13acaaf12c528d292b75cfce
Rony Vargas <vargaspihardo@gmail.com>
2021-06-06 12:14:00 -0400

    Generadores generator en es6 retornan valores dependiendo la logica utilizada

--- codigo.js
+++ codigo.js
@@ -39,3 +39,49 @@ hello();
 
 //es6
 // import { hello } from './module'
+
+
+
+
+
+//// GENERODORES (GENERATOR)
+//estos recuerdan el primer valor despues envian el otro
+function* helloworld(){
+    if(true){
+        yield 'Hello'
+    }
+    if(true){
+        yield 'world'
+    }
+}
+const generatorHello = helloworld();
+console.log(generatorHello.next().value)
+console.log(generatorHello.next().value)
+
+// function* fibonacci(){
+//     var fn1 = 1;
+//     var fn2 = 1;
+//     while (true){  
+//       var actual = fn2;
+//       fn2 = fn1;
+//       fn1 = fn1 + actual;
+//       var reset = yield actual;
+//       if (reset){
+//           fn1 = 1;
+//           fn2 = 1;
+//       }
+//     }
+//   }
+  
+//   var secuencia = fibonacci();
+//   console.log(secuencia.next().value);     // 1
+//   console.log(secuencia.next().value);     // 1
+//   console.log(secuencia.next().value);     // 2
+//   console.log(secuencia.next().value);     // 3
+//   console.log(secuencia.next().value);     // 5
+//   console.log(secuencia.next().value);     // 8
+//   console.log(secuencia.next().value);     // 13
+//   console.log(secuencia.next(true).value); // 1
+//   console.log(secuencia.next().value);     // 1
+//   console.log(secuencia.next().value);     // 2
+//   console.log(secuencia.next().value);     // 3
