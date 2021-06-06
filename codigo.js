9bf52e41118eb6b6c888464491d26cbc2113b872
Rony Vargas <vargaspihardo@gmail.com>
2021-06-06 11:19:29 -0400

    Agregue un modulo.js para exportar e importar nueva logica

--- codigo.js
+++ codigo.js
@@ -17,3 +17,25 @@ class calculator {
 
 const calc = new calculator();
 calc.sum(1,5);
+
+
+
+
+
+
+//MODULE 
+
+// Hay que tener en cuenta que al correr el programa en la terminal (como lo hace el profesor Oscar) y no en el browser, se está haciendo uso de Nodejs para ejecutar el código javaScript. Y como import de ES6 aún no es una sintaxis oficial en NodeJs, al tratar de correrlo nos arrojará un error del tipo: SyntaxError: Cannot use import statement outside a module.
+// Por lo que para exportar e importar módulos usando la terminal habría que hacer uso de la sintaxis vigente en Node:
+
+// para exportar
+// module.exports = hello;
+
+// para importar
+// const hello = require("./module");
+
+const hello = require('./module')
+hello();
+
+//es6
+// import { hello } from './module'
